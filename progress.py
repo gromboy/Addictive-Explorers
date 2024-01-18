50bb0d31540bd157e87698972f372ffaa708ea97
D1rosa <151547045+D1rosa@users.noreply.github.com>
2024-01-19 00:28:51 +0400

    DODELAT 2

--- progress.py
+++ progress.py
@@ -1,21 +1,24 @@
 def load_progress():
+    global hero, LEVEL
     with open('saves.txt', encoding='utf8') as f:
         text = f.readlines()
+
+    text = [i.strip() for i in text]
     for i in range(len(text)):
-        if i == 1:
+        if i == 0:
             hp = text[i].split(' = ')[1]
             hero.hp = hp
-        if i == 2:
+        if i == 1:
             dmg = text[i].split(' = ')[1]
             hero.dmg = dmg
-        if i == 3:
+        if i == 2:
             LEVEL = text[i].split(' = ')[1]
-        if i == 4:
-            if i == 1:
-                coins = text[i].split(' = ')[1]
-                hero.coins = coins
+        if i == 3:
+            coins = text[i].split(' = ')[1]
+            hero.coins = coins
 
 
 def save_progress():
     with open('saves.txt', 'wt', encoding='utf8') as f:
         f.write(f'hp = {hero.hp}\ndmg = {hero.dmg}\nlevel = {LEVEL}\ncoins = {hero.coins}')
+
